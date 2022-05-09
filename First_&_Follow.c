9eddaa7085889959608a07ed000f4ed25ee04780
Praneet-Botke <88475001+Praneet-Botke@users.noreply.github.com>
2022-05-09 20:35:58 +0530

    Update First_&_Follow.c

--- First_&_Follow.c
+++ First_&_Follow.c
@@ -1,140 +1,227 @@
-#include<stdio.h>
-#include<string.h>
-#include<conio.h>
+#include <stdio.h> 
+#include <ctype.h> 
+#include <string.h>
 
-#define max 20
-char prod[max][10];
-char ter[10], nt[10];
-char first[10][10], follow[10][10];
-int eps[10];
-int count = 0;
-int findpos(char ch) {
-  int n;
-  for (n = 0; nt[n] != '\0'; n++)
-    if (nt[n] == ch)
-      break;
-  if (nt[n] == '\0')
-    return 1;
-  return n;
-}
+void followfirst(char, int, int);
+void follow(char c);
+void findfirst(char, int, int);
+int count, n = 0;
+char calc_first[10][100];
+char calc_follow[10][100];
+int m = 0;
+char production[10][10];
+char f[10], first[10];
+int k;
+char ck;
+int e;
 
-int IsCap(char c) {
-  if (c >= 'A' && c <= 'Z')
-    return 1;
-  return 0;
-}
-void add(char * arr, char c) {
-  int i, flag = 0;
-  for (i = 0; arr[i] != '\0'; i++) {
-    if (arr[i] == c) {
-      flag = 1;
-      break;
-    }
-  }
-  if (flag != 1)
-    arr[strlen(arr)] = c;
-}
-void addarr(char * s1, char * s2) {
-  int i, j, flag = 99;
-  for (i = 0; s2[i] != '\0'; i++) {
-    flag = 0;
-    for (j = 0;; j++) {
-      if (s2[i] == s1[j]) {
-        flag = 1;
-        break;
-      }
-      if (j == strlen(s1) && flag != 1) {
-        s1[strlen(s1)] = s2[i];
-        break;
-      }
-    }
-  }
-}
-void addprod(char * s) {
-  int i;
-  prod[count][0] = s[0];
-  for (i = 3; s[i] != '\0'; i++) {
-    if (!IsCap(s[i]))
-      add(ter, s[i]);
-    prod[count][i - 2] = s[i];
-  }
-  prod[count][i - 2] = '\0';
-  add(nt, s[0]);
-  count++;
-}
-void findfirst() {
-  int i, j, n, k, e, n1;
-  for (i = 0; i < count; i++) {
-    for (j = 0; j < count; j++) {
-      n = findpos(prod[j][0]);
-      if (prod[j][1] == (char) 238)
-        eps[n] = 1;
-      else {
-        for (k = 1, e = 1; prod[j][k] != '\0' && e == 1; k++) {
-          if (!IsCap(prod[j][k])) {
-            e = 0;
-            add(first[n], prod[j][k]);
-          } else {
-            n1 = findpos(prod[j][k]);
-            addarr(first[n], first[n1]);
-            if (eps[n1] == 0)
-              e = 0;
-          }
+int main(int argc, char **argv)
+{
+    int jm = 0;
+    int km = 0;
+    int i, choice;
+    char c, ch;
+    count = 8;
+    // The Input grammar strcpy(production[0], "E=TR"); strcpy(production[1], "R=+TR");
+    strcpy(production[2], "R=#");
+    strcpy(production[5], "Y=#");
+    int kay;
+    char done[count];
+    int ptr = -1;
+    for (k = 0; k < count; k++)
+    {
+        strcpy(production[0], "E=TR"); 
+	strcpy(production[1], "R=+TR"); 
+	strcpy(production[2], "R=#"); 
+	strcpy(production[3], "T=FY"); 
+	strcpy(production[4], "Y=*FY"); 
+	strcpy(production[5], "Y=#"); 
+	strcpy(production[6], "F=(E)"); 
+	strcpy(production[7], "F=i");
+        for (kay = 0; kay < 100; kay++)
+        {
+            calc_first[k][kay] = '!';
         }
-        if (e == 1)
-          eps[n] = 1;
-      }
     }
-  }
-}
-void findfollow() {
-  int i, j, k, n, e, n1;
-  n = findpos(prod[0][0]);
-  add(follow[n], '$');
-  for (i = 0; i < count; i++) {
-    for (j = 0; j < count; j++) {
-      k = strlen(prod[j]) - 1;
-      for (; k > 0; k--) {
-        if (IsCap(prod[j][k])) {
-          n = findpos(prod[j][k]);
-          if (prod[j][k + 1] == '\0') // A -> aB
-          {
-            n1 = findpos(prod[j][0]);
-            addarr(follow[n], follow[n1]);
-          }
-          if (IsCap(prod[j][k + 1])) // A -> aBb
-          {
-            n1 = findpos(prod[j][k + 1]);
-            addarr(follow[n], first[n1]);
-            if (eps[n1] == 1) {
-              n1 = findpos(prod[j][0]);
-              addarr(follow[n], follow[n1]);
+    int point1 = 0, point2, xxx;
+    for (k = 0; k < count; k++)
+    {
+        c = production[k][0];
+        point2 = 0;
+        xxx = 0;
+        for (kay = 0; kay <= ptr; kay++)
+            if (c == done[kay])
+                xxx = 1;
+        if (xxx == 1)
+            continue;
+
+        findfirst(c, 0, 0);
+        ptr += 1;
+        done[ptr] = c;
+        printf("\n First(%c) = { ", c);
+        calc_first[point1][point2++] = c;
+        for (i = 0 + jm; i < n; i++)
+        {
+            int lark = 0, chk = 0;
+            for (lark = 0; lark < point2; lark++)
+            {
+                if (first[i] == calc_first[point1][lark])
+                {
+                    chk = 1;
+                    break;
+                }
             }
-          } else if (prod[j][k + 1] != '\0')
-            add(follow[n], prod[j][k + 1]);
+            if (chk == 0)
+            {
+                printf("%c, ", first[i]);
+                calc_first[point1][point2++] = first[i];
+            }
         }
-      }
+        printf("}\n");
+        jm = n;
+        point1++;
     }
-  }
+    printf("\n");
+    printf("-----------------------------------------------\n\n");
+    char donee[count];
+    ptr = -1;
+    for (k = 0; k < count; k++)
+    {
+
+        for (kay = 0; kay < 100; kay++)
+        {
+            calc_follow[k][kay] = '!';
+        }
+    }
+    point1 = 0;
+    int land = 0;
+    for (e = 0; e < count; e++)
+    {
+        ck = production[e][0];
+        point2 = 0;
+        xxx = 0;
+        for (kay = 0; kay <= ptr; kay++)
+            if (ck == donee[kay])
+                xxx = 1;
+        if (xxx == 1)
+            continue;
+        land += 1;
+        follow(ck);
+        ptr += 1;
+        donee[ptr] = ck;
+        printf(" Follow(%c) = { ", ck);
+        calc_follow[point1][point2++] = ck;
+        for (i = 0 + km; i < m; i++)
+        {
+            int lark = 0, chk = 0;
+            for (lark = 0; lark < point2; lark++)
+            {
+                if (f[i] == calc_follow[point1][lark])
+                {
+                    chk = 1;
+                    break;
+                }
+            }
+            if (chk == 0)
+            {
+                printf("%c, ", f[i]);
+                calc_follow[point1][point2++] = f[i];
+            }
+        }
+        printf(" }\n\n");
+        km = m;
+        point1++;
+    }
 }
-void main() {
-  char s[max], i;
-  printf("\nEnter the productions(type 'end' at the last of the production)\n");
-  scanf("%s", s);
-  while (strcmp("end", s)) {
-    addprod(s);
-    scanf("%s", s);
-  }
-  findfirst();
-  findfollow();
-  for (i = 0; i < strlen(nt); i++) {
-    printf("%c\t", nt[i]);
-    printf("%s", first[i]);
-    if (eps[i] == 1)
-      printf("%c\t", (char) 238);
+void follow(char c)
+{
+    int i, j;
+    if (production[0][0] == c)
+    {
+        f[m++] = '$';
+    }
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 2; j < 10; j++)
+        {
+            if (production[i][j] == c)
+            {
+                if (production[i][j + 1] != '\0')
+                {
+                    followfirst(production[i][j + 1], i, (j + 2));
+                }
+                if (production[i][j + 1] == '\0' && c != production[i][0])
+                {
+                    follow(production[i][0]);
+                }
+            }
+        }
+    }
+}
+void findfirst(char c, int q1, int q2)
+{
+    int j;
+    if (!(isupper(c)))
+    {
+        first[n++] = c;
+    }
+    for (j = 0; j < count; j++)
+    {
+        if (production[j][0] == c)
+        {
+            if (production[j][2] == '#')
+            {
+                if (production[q1][q2] == '\0')
+                    first[n++] = '#';
+                else if (production[q1][q2] != '\0' && (q1 != 0 || q2 != 0))
+                {
+                    findfirst(production[q1][q2], q1, (q2 + 1));
+                }
+                else
+                    first[n++] = '#';
+            }
+            else if (!isupper(production[j][2]))
+            {
+                first[n++] = production[j][2];
+            }
+            else
+            {
+                findfirst(production[j][2], j, 3);
+            }
+        }
+    }
+}
+void followfirst(char c, int c1, int c2)
+{
+    int k;
+    if (!(isupper(c)))
+        f[m++] = c;
     else
-      printf("\t");
-    printf("%s\n", follow[i]);
-  }
-  getch();
+    {
+        int i = 0, j = 1;
+        for (i = 0; i < count; i++)
+        {
+            if (calc_first[i][0] == c)
+                break;
+        }
+        while (calc_first[i][j] != '!')
+        {
+            if (calc_first[i][j] != '#')
+            {
+                f[m++] = calc_first[i][j];
+            }
+            else
+            {
+                if (production[c1][c2] == '\0')
+                {
+                    follow(production[c1][0]);
+                }
+                else
+                {
+                    followfirst(production[c1][c2], c1, c2 + 1);
+                }
+            }
+            j++;
+        }
+    }
 }
